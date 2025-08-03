10817a83d75606309abe0860253d24cd8de5be33
Suraj <suraj.2201082cs@iiitbh.ac.in>
2025-08-03 21:29:45 +0530

    adding clerk auth

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
 
       <div className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg z-50 flex flex-col md:flex-row items-center max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 overflow-hidden transition-[width] duration-300
        ${isOpen ? 'max-md:w-full' : 'max-md:w-0'}`}>
-        <XIcon className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer' onClick={() => setIsOpen(!isOpen)}/>
+        <XIcon className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer' onClick={() => setIsOpen( prevVal => !prevVal)}/>
         <Link onClick={() => {scrollTo(0,0), setIsOpen(false)}} to='/'>Home</Link>
         <Link onClick={() => {scrollTo(0,0), setIsOpen(false)}} to='/movies'>Movies</Link>
         <Link onClick={() => {scrollTo(0,0), setIsOpen(false)}} to='/'>Theaters</Link>
@@ -28,7 +28,7 @@ const Navbar = () => {
         <button className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>Login</button>
       </div>
 
-      <MenuIcon className='max-md:ml-4 md:hidden w-8 h-8 cursor-pointer' onClick={() => setIsOpen(!isOpen)}/>  
+      <MenuIcon className='max-md:ml-4 md:hidden w-8 h-8 cursor-pointer' onClick={() => setIsOpen(prevVal => !prevVal)}/>  
 
     </div>
   )
