5fc73d7f0bceafd714373985c5ebc1b8a827e749
Suraj <suraj.2201082cs@iiitbh.ac.in>
2025-08-03 22:47:58 +0530

    Adding static  hero section without admin prop

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HeroSection from '../components/HeroSection'
 
 const Home = () => {
   return (
     <div>
-      
+      <HeroSection></HeroSection>
     </div>
   )
 }
