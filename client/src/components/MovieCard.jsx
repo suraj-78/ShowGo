f8dd47e8b97d07c0e67d02336136d57f56da99fb
Suraj <suraj.2201082cs@iiitbh.ac.in>
2025-08-04 00:33:09 +0530

    MovieCard complete

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -1,3 +1,4 @@
+import { StarIcon } from 'lucide-react';
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
@@ -15,6 +16,15 @@ const MovieCard = ({movie}) => {
         <p className=' text-sm text-gray-400 mt-2'>
             {new Date(movie.release_date).getFullYear()} • {movie.genres.slice(0,2).map(genre => genre.name).join(" | ")} • {movie.runtime}
         </p>
+
+        <div className='flex items-center justify-between mt-4 pb-3'>
+            <button onClick={() => {navigate(`/movies/${movie._id}`); scroll(0, 0)}} className='px-4 py-2 text-xs bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>Buy Ticket</button>
+
+            <p className='flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1'>
+                <StarIcon className='w-4 h-4 text-primary fill-primary'/>
+                {movie.vote_average.toFixed(1)}
+            </p>
+        </div>
     </div>
   )
 }
