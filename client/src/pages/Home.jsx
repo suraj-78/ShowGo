1abe39c4d8d0cc51d6b25bb2798a0fbc78e6dd6f
Suraj <suraj.2201082cs@iiitbh.ac.in>
2025-08-04 13:05:00 +0530

    TrailerSection

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import FeatureSection from '../components/FeatureSection'
+import TrailerSection from '../components/TrailerSection'
 
 const Home = () => {
   return (
     <>
       <HeroSection></HeroSection>
       <FeatureSection/>
+      <TrailerSection/>
     </>
   )
 }
