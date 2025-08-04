520f5a8766e61670ddea72dfb59145092d290ab2
Suraj <suraj.2201082cs@iiitbh.ac.in>
2025-08-04 12:21:00 +0530

    Formatting the time section of the movieCard

--- client/src/components/FeatureSection.jsx
+++ client/src/components/FeatureSection.jsx
@@ -2,6 +2,8 @@ import { ArrowRight } from 'lucide-react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import BlurCircle from './BlurCircle';
+import { dummyShowsData } from '../assets/assets';
+import MovieCard from './MovieCard';
 
 const FeatureSection = () => {
     const navigate = useNavigate();
@@ -18,8 +20,10 @@ const FeatureSection = () => {
         </div>
 
 
-        <div>
-
+        <div className='flex flex-wrap max-sm:justify-center gap-8 mt-8'>
+            {dummyShowsData.slice(0,4).map((show) => (
+                <MovieCard key={show._id} movie={show}/>
+            ))}
         </div>
 
         <div className='flex justify-center mt-20'>
---
+++ client/src/lib/timeFormat.js
@@ -0,0 +1,9 @@
+const timeFormat = (minutes) => {
+    const hours = Math.floor(minutes / 60);
+    const minutesRemaining = minutes % 60;
+
+    return `${hours}h ${minutesRemaining}m`
+}
+
+
+export default timeFormat;
