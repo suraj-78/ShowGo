3488563e9fb7d97eae4760b52ab8bfb1831498ad
Suraj <suraj.2201082cs@iiitbh.ac.in>
2025-08-08 00:08:13 +0530

    adding favourite page and setting up the movie detail page

--- client/src/pages/MovieDetail.jsx
+++ client/src/pages/MovieDetail.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { dummyDateTimeData, dummyShowsData } from '../assets/assets';
 
 const MovieDetail = () => {
-  return (
-    <div>
-      
+  const {id} = useParams()
+  const [show, setShow] = useState(null);
+
+  const getShow = async () => {
+    const show = dummyShowsData.find(show => show._id === id);
+    setShow({
+      movie : show, 
+      dateTime : dummyDateTimeData
+    })
+  }
+
+
+  // using useEffect so that whenever the show changes the component shoud rerender
+
+  useEffect( () => {
+    getShow();
+  }, [id])
+  return show ? (
+    <div className=''>
+        <div className=''>
+
+        </div>
     </div>
-  )
-}
+  ) : <div>Loading....</div>
+} 
 
 export default MovieDetail
