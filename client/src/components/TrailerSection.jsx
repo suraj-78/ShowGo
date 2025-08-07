f03d8f55194b34a380816f0833251ecd21776a2b
Suraj <suraj.2201082cs@iiitbh.ac.in>
2025-08-07 17:41:32 +0530

    completed multiple trailer section

--- client/src/components/TrailerSection.jsx
+++ client/src/components/TrailerSection.jsx
@@ -5,7 +5,6 @@ import BlurCircle from './BlurCircle'
 import { PlayCircleIcon } from 'lucide-react'
 
 const TrailerSection = () => {
-
     const [currentTrailer, setCurrentTrailer]  = useState(dummyTrailers[0]);
   return (
     <div className='px-6 md:px-16 lg:px-24 xl:px-44 py-20 overflow-hidden'>
@@ -26,9 +25,11 @@ const TrailerSection = () => {
 
       <div className=' group grid grid-cols-4 gap-4 md:gap-8 mt-8 max-w-3xl mx-auto'>
         {dummyTrailers.map( (trailer) => (
-          <div>
+          <div key={trailer.image} className=' relative group-hover:not-hover:opacity-50 hover:-translate-y-1 duration-300 translation max-md:h-60 cursor-pointer' 
+          onClick={() => { setCurrentTrailer(trailer)}}>
+
             <img src={trailer.image} alt='trailer' className=' rounded-lg w-full h-full object-cover brightness-75'/>
-            <PlayCircleIcon className=' absolute top-1/2 left-1/2 w-5 md:w-8 h-5 md:h-12'/>
+            <PlayCircleIcon className=' absolute top-1/2 left-1/2 w-5 md:w-8 h-5 md:h-12 transform -translate-x-1/2 -translate-y-1/2'/>
           </div>
         ))}
       </div>
