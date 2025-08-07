7b2d618a362d827b306a24532c1b4da7c9877ca2
Suraj <suraj.2201082cs@iiitbh.ac.in>
2025-08-07 23:41:35 +0530

    styles to the movie section

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -4,9 +4,9 @@ import MovieCard from '../components/MovieCard'
 
 const Movies = () => {
   return dummyShowsData.length > 0 ? (
-    <div>
-      <h1>Now Showing</h1>
-      <div>
+    <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]'>
+      <h1 className='text-lg font-medium my-4'>Now Showing</h1>
+      <div className='flex flex-wrap max-sm:justify-center gap-8'>
         {dummyShowsData.map( (movie) => (
         <MovieCard movie={movie} key={movie._id}/>
       ))}
