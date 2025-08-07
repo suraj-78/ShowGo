d4d7dae3ad06cf115e2930bddc56eec22b53bcab
Suraj <suraj.2201082cs@iiitbh.ac.in>
2025-08-07 23:38:19 +0530

    movie section

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { dummyShowsData } from '../assets/assets'
+import MovieCard from '../components/MovieCard'
 
 const Movies = () => {
-  return (
+  return dummyShowsData.length > 0 ? (
+    <div>
+      <h1>Now Showing</h1>
+      <div>
+        {dummyShowsData.map( (movie) => (
+        <MovieCard movie={movie} key={movie._id}/>
+      ))}
+      </div>
+    </div>
+  ) : (
     <div>
       
     </div>
