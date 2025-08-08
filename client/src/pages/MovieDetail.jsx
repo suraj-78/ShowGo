e2c2ba9d4d1b858c51000a5a742e33ce4edc3f4d
Suraj <suraj.2201082cs@iiitbh.ac.in>
2025-08-08 13:22:30 +0530

    poster section in movie page

--- client/src/pages/MovieDetail.jsx
+++ client/src/pages/MovieDetail.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { dummyDateTimeData, dummyShowsData } from '../assets/assets';
+import BlurCircle from '../components/BlurCircle';
+import { StarIcon } from 'lucide-react';
+import timeFormat from '../lib/timeFormat';
 
 const MovieDetail = () => {
   const {id} = useParams()
@@ -13,17 +16,34 @@ const MovieDetail = () => {
       dateTime : dummyDateTimeData
     })
   }
-
-
   // using useEffect so that whenever the show changes the component shoud rerender
 
   useEffect( () => {
     getShow();
   }, [id])
-  return show ? (
-    <div className=''>
-        <div className=''>
 
+
+  return show ? (
+    <div className='px-6 md:px-16 lg:px-40 pt-30 md:pt-50'>
+        <div className=' flex flex-col md:flex-row gap-8 max-w-6xl mx-auto'>
+            <img src={show.movie.poster_path} alt='' className='max-md:mx-auto rounded-xl h-104 max-w-70 object-cover'/>
+
+            <div className='relative flex flex-col gap-3'>
+              <BlurCircle top='-100px' left='-100px'/>
+              <p className='text-primary'>ENGLISH</p>
+              <h1 className='text-4xl font-semibold max-w-96 text-balance'>{show.movie.title}</h1>
+              <div className='flex items-center gap-2 text-gray-300'>
+                <StarIcon className='w-5 h-5 text-primary fill-primary'/>
+                {show.movie.vote_average.toFixed(1)} User Rating
+
+              </div>
+
+              <p className='text-gray-400 mt-2 text-sm leading-tight max-w-xl'>{show.movie.overview}</p>
+
+              <p>
+                {timeFormat(show.movie.runtime)} • {show.movie.genres.map( genre => genre.name).join(", ")} • {show.movie.release_date.split("-")[0]}
+              </p>
+            </div>
         </div>
     </div>
   ) : <div>Loading....</div>
