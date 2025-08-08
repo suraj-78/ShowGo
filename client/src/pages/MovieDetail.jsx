552d208b54a2fbf3ded97b10463f3921af9ac8a0
Suraj <suraj.2201082cs@iiitbh.ac.in>
2025-08-08 13:38:43 +0530

    styling the watch trailer and buy ticket button

--- client/src/pages/MovieDetail.jsx
+++ client/src/pages/MovieDetail.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { dummyDateTimeData, dummyShowsData } from '../assets/assets';
 import BlurCircle from '../components/BlurCircle';
-import { StarIcon } from 'lucide-react';
+import { Heart, PlayCircleIcon, StarIcon } from 'lucide-react';
 import timeFormat from '../lib/timeFormat';
 
 const MovieDetail = () => {
@@ -43,6 +43,19 @@ const MovieDetail = () => {
               <p>
                 {timeFormat(show.movie.runtime)} • {show.movie.genres.map( genre => genre.name).join(", ")} • {show.movie.release_date.split("-")[0]}
               </p>
+              
+              <div className='flex items-center flex-wrap gap-4 mt-4'>
+                <button className='flex items-center gap-2 px-7 py-3 text-sm bg-gray-800 hover:bg-gray-900 
+                transition rounded-md font-medium cursor-pointer active:scale-95'>
+                  <PlayCircleIcon className='w-5 h-5'/>
+                  Watch Trailer
+                </button>
+                <a href='' className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer active:scale-95'>Buy Ticket</a>
+                <button className=' bg-gray-700 p-2.5 rounded-full transition cursor-pointer active:scale-95'>
+                  <Heart className={'w-5 h-5'}/>
+                </button>
+              </div>
+
             </div>
         </div>
     </div>
