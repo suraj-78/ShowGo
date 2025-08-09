dea37c14afcba622d807916594d87562899a7323
Suraj <suraj.2201082cs@iiitbh.ac.in>
2025-08-09 18:01:23 +0530

    core functionality of the seatLayout

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { dummyDateTimeData, dummyShowsData } from '../assets/assets';
 
 const SeatLayout = () => {
-  return (
+
+  const {id, date} = useParams();
+
+  const [selectedSeats, setSelectedSeats] = useState([]);
+  const [selectedTime, setSelectedTime] = useState(null);
+
+  const [show, setShow] = useState(null);
+
+  // function to fetch the show data from the id
+  const getShow = async() => {
+    const show = dummyShowsData.find(show => show._id === id);
+    if(show) {
+      setShow({
+        movie : show,
+        dateTime : dummyDateTimeData
+      })
+    }
+  }
+
+  useEffect(() => {
+    getShow();
+  }, [])
+
+  const navigate = useNavigate();
+  return show ? (
     <div>
+
       
     </div>
+  ) : (
+    <Loading />
   )
 }
 
