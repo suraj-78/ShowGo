09514044115d1c3abde077592cc087305de83507
Suraj <suraj.2201082cs@iiitbh.ac.in>
2025-08-10 16:21:23 +0530

    adding the myBooking skeleton

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyBookingData } from '../assets/assets';
+import Loading from '../components/Loading';
+import BlurCircle from '../components/BlurCircle';
 
 const MyBookings = () => {
-  return (
+
+  const currency = import.meta.env.VITE_CURRENCY
+
+  const [bookings, setBookings] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getMyBookings = async() => {
+    setBookings(dummyBookingData);
+    setIsLoading(false);
+  }
+
+  useEffect( () => {
+    getMyBookings();
+  }, []);
+
+
+  return !isLoading ? (
+    <div className='relative px-6 md:px-16 lg:px-40 pt-30 md:pt-40 min-h-[80vh]'>
+      <BlurCircle top='100px' left='100px'/>
+      <div>
+        <BlurCircle bottom='0px' left='600px'/>
+      </div>
+      <h1 className='text-lg font-semibold mb-4'>My Bookings</h1>
+    </div>
+  ) : (
     <div>
-      
+      <Loading/>
     </div>
   )
 }
