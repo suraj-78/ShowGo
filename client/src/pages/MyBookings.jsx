bf1beb806281e975dec058dc832c3c11108a6b40
Suraj <suraj.2201082cs@iiitbh.ac.in>
2025-08-10 16:53:31 +0530

    MyBooking Card

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { dummyBookingData } from '../assets/assets';
 import Loading from '../components/Loading';
 import BlurCircle from '../components/BlurCircle';
+import timeFormat from '../lib/timeFormat';
 
 const MyBookings = () => {
 
@@ -27,6 +28,20 @@ const MyBookings = () => {
         <BlurCircle bottom='0px' left='600px'/>
       </div>
       <h1 className='text-lg font-semibold mb-4'>My Bookings</h1>
+
+      {bookings.map((item, index) => (
+        <div key={index} className='flex flex-col md:flex-row justify-between bg-primary/8 border border-primary/20 rounded-lg mt-4 p-2 max-w-3xl'>
+          <div className='flex flex-col md:flex-row'>
+            <img src={item.show.movie.poster_path} alt='' className=' md:max-w-45 aspect-video h-auto object-cover object-bottom rounded'/>
+            <div className='flex flex-col p-4'>
+              <p className=' text-lg font-semibold'>{item.show.movie.title}</p>
+              <p className=' text-gray-400 text-sm'>{timeFormat(item.show.movie.runtime)}</p>
+              <p className='text-gray-400 text-sm mt-auto'>{item.show.showDateTime}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+
     </div>
   ) : (
     <div>
