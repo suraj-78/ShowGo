a7f75ee097edc801ff5c133f494415c818884a53
Suraj <suraj.2201082cs@iiitbh.ac.in>
2025-10-02 01:28:55 +0530

    updated adminSidebar

--- client/src/components/admin/AdminSidebar.jsx
+++ client/src/components/admin/AdminSidebar.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
+import { LayoutDashboardIcon, ListCollapseIcon, ListIcon, PlusSquareIcon } from 'lucide-react'
+import { NavLink } from 'react-router-dom'
 
 const AdminSidebar = () => {
+
+    const user = {
+        firstName : 'Admin',
+        lastName : 'User',
+        imageUrl: assets.profile
+    }
+
+    const adminNavlinks = [
+        { name : 'Dashboard', path : '/admin' , icon : LayoutDashboardIcon},
+        { name : 'Add Shows', path : '/admin/add-shows', icon : PlusSquareIcon },
+        { name : 'List Shows', path : '/admin/list-shows', icon : ListIcon},
+        { name : 'List Bookings', path : '/admin/list-bookings', icon : ListCollapseIcon},
+    ]
   return (
-    <div>
-      
+    <div className='h-[calc(100hv-64px)] md:flex flex-col items-center pt-8 max-w-13 md:max-w-60 w-full border-r border-gray-300/20 text-sm'>
+      <img className='h-9 md:h-14 w-9 md:w-14 rounded-full mx-auto' src={user.imageUrl} alt='sidebar'/>
+      <p className='mt-2 text-base max-md:hidden'>{user.firstName} {user.lastName}</p>
+      <div>
+        {adminNavlinks.map((link, index) => (
+            <NavLink className={({ isActive }) => `relative flex items-center max-md:justify-center gap-2 w-full py-2.5 min-md:pl-10 first:mt-6
+             text-gray-400 ${isActive && 'bg-primary/15 text-primary group'}`}>
+                {
+                    ({ isActive }) => (
+                        <>
+                            <link.icon className='w-5 h-5'/>
+                            <p>
+
+                            </p>
+                            <span className='{}'/>
+                        </>
+                    )
+                }
+            </NavLink>
+        ))}
+      </div>
     </div>
   )
 }
