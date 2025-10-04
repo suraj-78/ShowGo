60371fd07f88847a3d5b2e94c1a924260299e547
Suraj <suraj.2201082cs@iiitbh.ac.in>
2025-10-04 23:30:11 +0530

    completed admin dashboard

--- client/src/pages/admin/AddShow.jsx
+++ client/src/pages/admin/AddShow.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { dummyShowsData } from '../../assets/assets';
-import { CheckIcon, StarIcon } from 'lucide-react';
+import { CheckIcon, DeleteIcon, StarIcon } from 'lucide-react';
 import Loading from '../../components/Loading';
 import Title from '../../components/admin/Title.JSX';
 import { kConverter } from '../../lib/kConverter';
@@ -18,9 +18,39 @@ const AddShow = () => {
         setNowPlayingMovies(dummyShowsData)
     };
 
+    const handleDateTimeAdd = ()=>{
+        if(!dateTimeInput) return;
+        const[date ,time]=dateTimeInput.split("T");
+        if(!date|| !time) return;
+
+        setdateTimeSelection((prev)=>{
+            const times = prev[date]||[];
+            if(!times.includes(time)){
+                return{...prev , [date]:[...times,time]};
+            }
+            return prev;
+        });   
+    };
+
+    const handleRemoveTime = (date, time) => {
+        setDateTimeSelection((prev) => {
+        const filteredTimes = prev[date].filter((t) => t !== time);
+        if (filteredTimes.length === 0) {
+        const { [date]: _, ...rest } = prev;
+        return rest;
+        }
+        return {
+        ...prev,
+        [date]: filteredTimes,
+        };
+    });
+};
+
+
+
     useEffect(()=>{
    fetchNowPlayingMovies();
-    });
+    }, []);
 
 
 return nowPlayingMovies.length > 0 ? (
@@ -64,7 +94,7 @@ return nowPlayingMovies.length > 0 ? (
         
         <div className=' mt-8'>
             <label className=' block text-sm font-medium mb-2'>Show Price</label>
-            <div>
+            <div className=' inline-flex items-center gap-2 border border-y-gray-600 px-3 py-2 rounded-md'>
                 <p>{currency}</p>
                 <input min={0} type='number' value={showPrice} onChange={(e) => 
                 setShowPrice(e.target.value)} placeholder='Enter the show price'
@@ -72,7 +102,56 @@ return nowPlayingMovies.length > 0 ? (
             </div> 
         </div>
 
+       {/* Date & Time Selection */}
+        <div className="mt-6">
+        <label className="block text-sm font-medium mb-2">
+            Select Date and Time
+        </label>
+        <div className="inline-flex gap-5 border border-gray-600 p-1 pl-3 rounded-lg">
+            <input 
+            type="datetime-local" 
+            value={dateTimeInput} 
+            onChange={(e) => setDateTimeInput(e.target.value)} 
+            className="outline-none rounded-md" 
+            />
+            <button 
+            onClick={handleDateTimeAdd} 
+            className="bg-primary/80 text-white px-3 py-2 text-sm rounded-lg hover:bg-primary cursor-pointer"
+            >
+            Add Time
+            </button>
+        </div>
+        </div>
 
+{/* Display Selected Times */}
+    {Object.keys(dateTimeSelection).length > 0 && (
+    <div className="mt-6">
+        <h2 className="mb-2">Selected Date-Time</h2>
+        <ul className="space-y-3">
+        {Object.entries(dateTimeSelection).map(([date, times]) => (
+            <li key={date}>
+            <div className="font-medium">{date}</div>
+            <div className="flex flex-wrap gap-2 mt-1 text-sm">
+                {times.map((time) => (
+                <div
+                    key={time}
+                    className="border border-primary px-2 py-1 flex items-center rounded"
+                >
+                    <span>{time}</span>
+                    <DeleteIcon
+                    onClick={() => handleRemoveTime(date, time)}
+                    width={15}
+                    className="ml-2 text-red-500 hover:text-red-700 cursor-pointer"
+                    />
+                </div>
+                ))}
+          </div>
+        </li>
+      ))}
+    </ul>
+  </div>
+)}
+    <button className=' bg-primary text-white px-8 py-2 mt-6 rounded hover:bg-primary/90 transition-all cursor-pointer'>Add Show</button>
   </>
 ) : (
   <Loading />
