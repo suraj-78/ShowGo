4019e03fe101b0f7b49e69edb21b4d080c6dfa4d
Suraj <suraj.2201082cs@iiitbh.ac.in>
2025-10-04 12:48:19 +0530

    Added AddShow page

--- client/src/pages/admin/AddShow.jsx
+++ client/src/pages/admin/AddShow.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyShowsData } from '../../assets/assets';
+import { StarIcon } from 'lucide-react';
+import Loading from '../../components/Loading';
+import Title from '../../components/admin/Title.JSX';
 
 const AddShow = () => {
-  return (
-    <div>
-      
+    const currency = import.meta.env.VITE_CURRENCY
+    const [nowPlayingMovies , setNowPlayingMovies] = useState([]);
+    const [selectedMovie  , setSelectedMovie]= useState(null);
+    const[dateTimeSelection ,setdateTimeSelection] = useState({});
+    const[dateTimeInput , setDateTimeInput]=useState("");
+    const[showPrice , setShowPrice]=useState("");
+
+
+    const fetchNowPlayingMovies = async()=>{
+        setNowPlayingMovies(dummyShowsData)
+    };
+
+    useEffect(()=>{
+   fetchNowPlayingMovies();
+    });
+
+
+return nowPlayingMovies.length > 0 ? (
+  <>
+    <Title text1="Add" text2="Shows" />
+    <p className="mt-10 text-lg font-medium">Now Playing Movies</p>
+    <div className="overflow-x-auto pb-4">
+      <div className="group flex flex-wrap gap-4 mt-4 w-max">
+        {nowPlayingMovies.map((movie) => (
+          <div
+            key={movie.id}
+            className={`relative max-w-40 cursor-pointer 
+              group-hover:not-hover:opacity-40 hover:-translate-y-1 
+              transition duration-300`}
+          >
+            <div className="relative rounded-lg overflow-hidden">
+              <img
+                src={movie.poster_path}
+                alt=""
+                className="w-full object-cover brightness-90"
+              />
+              <div className="text-sm flex items-center justify-between p-2 bg-black/70 w-full absolute bottom-0 left-0">
+                <p className="flex items-center gap-1 text-gray-400">
+                  <StarIcon className="w-4 h-4 text-primary fill-primary" />
+                  {movie.vote_average.toFixed(1)}
+                </p>
+                <p className="text-gray-300">{movie.vote_count} Votes</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  </>
+) : (
+  <Loading />
+)
 }
+
+
 
 export default AddShow
